9e762497ec4cd0d1422280e803f7b10e819be865
Russell Fraze <russell.fraze@gmail.com>
2022-07-28 20:27:28 -0700

    make blocks

--- index.js
+++ index.js
@@ -2,22 +2,49 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
 
+//make the block
 class Block {
     constructor(xAxis, yAxis) {
-        this.bottomLeft = [xAxis,yAxis]
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
     }
-        
+
 }
 
-function addBlock() {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.bottom = '50px'
-    block.style.left = '100px'
-    grid.appendChild(block)
-     
+const blocks = [
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+    // new Block(10,270),
+]
+
+//draw the block
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.bottom = '50px'
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+    }
+
 }
 
-addBlock()
+addBlocks()
 
 
