a072b70789c0b2708d5181bb612624ef6c1100a0
Russell Fraze <russell.fraze@gmail.com>
2022-08-01 09:04:17 -0700

    user square and ball added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
+const boardWidth = 560
+
+const userStart = [230, 10]
+let currentPosition = userStart
 
 //make the block
 class Block {
@@ -14,22 +18,21 @@ class Block {
 }
 
 const blocks = [
-    new Block(10,270),
-    new Block(120,270),
-    new Block(230,270),
-    new Block(340,270),
-    new Block(450,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
-    // new Block(10,270),
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
 ]
 
 //draw the block
@@ -48,3 +51,40 @@ function addBlocks() {
 addBlocks()
 
 
+//add user
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//drawe the user
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
+
+//move user
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawUser()
+            }
+            break
+        case 'ArrowRight':
+            if (currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10
+                drawUser()
+            }
+            break
+
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+
+// add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+grid.appendChild(ball)
