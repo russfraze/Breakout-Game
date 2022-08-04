53f6151ebbf3be71df8fbb39119a58f778ef7577
Russell Fraze <russell.fraze@gmail.com>
2022-08-04 16:14:40 -0700

    move ball

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 20
 const boardWidth = 560
+const ballDiameter = 20
+let timerId
+let xDirection = 2
+let yDirection = 2
 
 const userStart = [230, 10]
 let currentPosition = userStart
+
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+
+
 
 //make the block
 class Block {
@@ -57,10 +66,16 @@ user.classList.add('user')
 drawUser()
 grid.appendChild(user)
 
-//drawe the user
+//draw the user
 function drawUser() {
     user.style.left = currentPosition[0] + 'px'
     user.style.bottom = currentPosition[1] + 'px'
+}
+
+//draw the ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
 }
 
 //move user
@@ -87,4 +102,28 @@ document.addEventListener('keydown', moveUser)
 // add ball
 const ball = document.createElement('div')
 ball.classList.add('ball')
+drawBall()
 grid.appendChild(ball)
+
+//move ball 
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+}
+
+timerId = setInterval(moveBall, 30)
+
+// check for collisions
+function checkForCollisions() {
+    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter)) {
+        changeDirection()
+    }
+}
+
+function changeDirection() {
+    if (xDirection === 2 && yDirection ===2 ) {
+        yDirection = -2
+        return
+    }
+}
