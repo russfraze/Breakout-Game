3628ab21d8cc1116bada03ebea728dde241f0856
Russell Fraze <russell.fraze@gmail.com>
2022-08-05 17:08:23 -0700

    change direction

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector("#score")
 const blockWidth = 100
 const blockHeight = 20
 const boardWidth = 560
+const boardHeight = 300
 const ballDiameter = 20
 let timerId
 let xDirection = 2
@@ -110,20 +112,44 @@ function moveBall() {
     ballCurrentPosition[0] += xDirection
     ballCurrentPosition[1] += yDirection
     drawBall()
+    checkForCollisions()
 }
 
 timerId = setInterval(moveBall, 30)
 
 // check for collisions
 function checkForCollisions() {
-    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter)) {
+    if (
+        ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+        ballCurrentPosition[0] <= 0
+        ) {
         changeDirection()
+    }
+
+    //check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.textContent = "you lose!"
+        document.removeEventListener('keydown', moveUser)
     }
 }
 
 function changeDirection() {
-    if (xDirection === 2 && yDirection ===2 ) {
+    if (xDirection === 2 && yDirection === 2 ) {
         yDirection = -2
+        return
+    }
+    if (xDirection === 2 && yDirection === -2){
+        xDirection = -2
+        return
+    }
+    if (xDirection === -2 && yDirection === -2) {
+        yDirection = 2
+        return
+    }
+    if (xDirection === -2 && yDirection === 2) {
+        xDirection = 2
         return
     }
 }
